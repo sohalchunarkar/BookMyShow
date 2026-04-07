d2ac5108f0440c878f8a17e1de17967bcba10786
sohal <sohalchunarkar8@gmail.com>
2026-04-08 00:55:34 +0530

    change contraint for hall name before it was unique for all hall name but not now

--- src/main/java/com/bookmyshow/bms/model/Hall.java
+++ src/main/java/com/bookmyshow/bms/model/Hall.java
@@ -8,7 +8,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 import com.fasterxml.jackson.annotation.JsonBackReference;
 
-import jakarta.persistence.Column;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -31,7 +31,6 @@ public class Hall {
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
-    @Column(unique = true)
     private String hallname;
     private int hallcapacity;
     private int Hallrows;
